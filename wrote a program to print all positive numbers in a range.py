24642914ec352d4f0cf59b446546f91d04fc5a43
R.K Ayushman Singh <97459871+Dreamerx17@users.noreply.github.com>
2022-02-17 15:52:43 +0530

    Create wrote a program to print all positive numbers in a range.py

---
+++ wrote a program to print all positive numbers in a range.py
@@ -0,0 +1,23 @@
+list1=[12,-7,5,64,-14]
+
+for num in list1:
+
+      if num>=0:
+        print(num,end=",")
+        
+        """
+        otput
+        12,5,64,
+        """
+        
+lsit1=[12,14,-95,3]
+list2=[]
+for num in list1:
+    if(num>=0):
+        list2.append(num)
+print(list2) 
+
+ """
+ otput
+ [12, 14, 3]
+  """
